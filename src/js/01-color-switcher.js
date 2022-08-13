7c3c777d6787ed9cb0d1191d9220d363b142b1db
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-08-13 21:22:56 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,46 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+};
 
+
+let timerId = null;
+
+disableBtn(refs.stopBtn);
+
+refs.startBtn.addEventListener('click', onClickStart);
+
+
+function onClickStart(e) {
+    enableBtn(refs.stopBtn)
+    e.target.disabled = true;
+    refs.stopBtn.addEventListener('click', onClickStop);
+    changeBodyColor(getRandomHexColor());
+    timerId = setInterval(() => {
+        color = getRandomHexColor();
+        changeBodyColor(color)
+    }, 1000)
+}
+
+function onClickStop(e) {
+    enableBtn(refs.startBtn);
+    e.target.disabled = true;
+    clearInterval(timerId);
+}
+
+function disableBtn(btnRef) {
+    btnRef.disabled = true
+}
+
+function enableBtn(btnRef) {
+    btnRef.disabled = false
+}
+
+function changeBodyColor(color) {
+    refs.body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
